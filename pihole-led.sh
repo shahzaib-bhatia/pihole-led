067a4f01d8050109d37f48cde896044ae3257574
Shahzaib Bhatia <shahzaib.bhatia@rackspace.com>
2017-08-29 23:33:46 +0000

    Cannibalize original. Use power/activity LEDs.

--- pihole-led.sh
+++ pihole-led.sh
@@ -1,28 +1,59 @@
 #!/bin/bash
-###########
-### This pihole-led script is built for the odroid c1+ using wiringPi here:
-### https://github.com/hardkernel/wiringPi
-### Built for pihole log's however you can modify as needed of course.
-### I use header pin 9 for the ground and pin 11 for gpio.
-### header 11 is equal to export gpio=88 or wiringPi pin=0 as used below.
-###########
-### Version: 		0.1
-### Author: 		Daniel Stinebaugh
-### Attribution:	http://www.stinebaugh.info/get-led-alerts-for-each-blocked-ad-using-pi-hole/
-### Date: 			03/19/2016
-### License: 		Copyleft. Enjoy!
-###########
-pin=0
-gpio mode $pin out
-# Defaults the pin to low when starting
-gpio write $pin 0
-tailf /var/log/pihole.log | while read INPUT
+
+trap 'quit' SIGINT SIGQUIT SIGTERM
+
+ACT='/sys/class/leds/led0'
+PWR='/sys/class/leds/led1'
+
+quit()
+{
+  reset_leds
+  exit 1
+}
+
+init_leds()
+{
+  echo oneshot > ${ACT}/trigger
+  echo oneshot > ${PWR}/trigger
+  sleep 0.1
+  echo 1 > ${ACT}/invert
+  sleep 0.1
+  echo 1 > ${ACT}/shot
+  echo 1 > ${PWR}/shot
+}
+
+reset_leds()
+{
+
+  echo mmc0 > ${ACT}/trigger
+  echo input > ${PWR}/trigger
+}
+
+if [[ "$(systemctl show dnsmasq --property=ActiveState)" != "ActiveState=active" ]]
+then
+  quit
+fi
+
+init_leds
+
+tail -n1 -f /var/log/pihole.log | while read INPUT
 do
-   if [[ "$INPUT" == *"/etc/pihole/gravity.list"* ]]; then
-       gpio write $pin 1
-       sleep 0.2
-       gpio write $pin 0
-#       echo "another ad bites the dust!"
-       sleep 0.1
+  case $INPUT in
+
+  *' /etc/pihole/gravity.list '*)
+    echo 1 > ${PWR}/shot
+    ;;
+
+  *' forwarded '*)
+    echo 1 > ${ACT}/shot
+    ;;
+
+  *)
+    if [[ "$(systemctl show dnsmasq --property=ActiveState)" != "ActiveState=active" ]]
+    then
+      quit
     fi
+    ;;
+
+  esac
 done
